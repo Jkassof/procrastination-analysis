f3e5ae595a841c8a8e591e5e1c53d1826ffa8a04
Jeff Weltman <jeffweltman@outlook.com>
2017-11-30 13:37:22 -0600

    Created simple merge script, added output

---
+++ data/merging-hdi-and-procrastination.R
@@ -0,0 +1,8 @@
+# Importing tidy data and hdi_table
+final_procras <- read.csv("./data/TidyProcrastination.csv",header=TRUE)
+hdi_table <- read.csv("./data/hdi_table.csv",header=TRUE)
+
+# Merging Data
+
+all_data <- merge(x=final_procras,y=hdi_table, by="CntryOfRes",all=TRUE)
+write.csv(all_data,"./data/all_data.csv",row.names=FALSE)
