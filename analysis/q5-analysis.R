981e3b785678d8683710e5e855533de83b97af85
Jordan Kassof <jkassof@gmail.com>
2017-12-04 20:48:07 -0500

    Add package message suppression and pull out plotting code to report

--- analysis/q5-analysis.R
+++ analysis/q5-analysis.R
@@ -1,9 +1,11 @@
+suppressPackageStartupMessages({
 library(ggplot2)
 library(dplyr)
 library(knitr)
 library(kableExtra)
 library(highcharter)
 library(ggiraph)
+})
 
 # reading in data
 
@@ -24,13 +26,6 @@ top15_aip_hdi <- top15_aip %>%
   inner_join(hdi_table) %>%
   select(-HDI)
 
-ggplot(top15_aip_hdi,aes(x=CntryOfRes, y=CntryMeanAIP,fill=hdi_group))+
-  geom_bar(aes(reorder(CntryOfRes,CntryMeanAIP)), stat="identity")+
-  ggtitle("Top 15 Most Procrastinating Nations (By Average AIP Score)")+
-  xlab("Country")+ylab("Average AIP Score")+
-  scale_fill_brewer(palette="Dark2")+
-  coord_flip()
-
 top15_gp <- all_data %>%
   filter(!is.na(CntryOfRes)) %>%
   select(CntryOfRes,GPMean) %>%
@@ -44,20 +39,13 @@ top15_gp_hdi <- top15_gp %>%
   inner_join(hdi_table) %>%
   select(-HDI)
 
-ggplot(top15_gp_hdi,aes(x=CntryOfRes, y=CntryMeanGP,fill=hdi_group))+
-  geom_bar(aes(reorder(CntryOfRes,CntryMeanGP)), stat="identity")+
-  ggtitle("Top 15 Most Procrastinating Nations (By Average GP Score)")+
-  xlab("Country")+ylab("Average GP Score")+
-  scale_fill_brewer(palette="Accent")+
-  coord_flip()
-
 # which countries are in the top 15 across both tests
 top_procrastinators <- top15_aip_hdi %>%
   select(CntryOfRes,CntryMeanAIP,hdi_group) %>%
   inner_join(top15_gp_hdi) %>%
   arrange(CntryOfRes)
 
-top_procrastinators.long <- melt(top_procrastinators)
+top_procrastinators.long <- reshape2::melt(top_procrastinators)
 
 top_procrastinators.long <- top_procrastinators.long %>% arrange(order(desc(value)))
 
@@ -77,24 +65,6 @@ knitr::kable(top_procrastinators,"html",row.names=FALSE) %>%
   kableExtra::kable_styling(bootstrap_options = c("striped","condensed"), 
                             full_width=F, 
                             position="left")
-
-# Is there a relationship between age and income?
-
-ggplot(data = all_data, aes(x=AnnualIncome, y=Age, color = Gender))+
-  geom_point(size=2)+
-  xlab("Annual Income in USD")+ylab("Age In Years")+
-  ggtitle("Examining Relationship Between Age and Income")
-
-paste("There is no apparent relationship between Age and Income.")
-
-# Is there a relationship between life satisfaction (SWLS) and HDI?
-
-ggplot(data = all_data, aes(x=HDI, y=SWLSMean, color = Gender))+
-  geom_point(size=2)+
-  xlab("Human Development Index")+ylab("Mean Satisfaction With Life Score")+
-  ggtitle("Examining Relationship Between HDI and Satisfaction With Life")
-
-# Sure does not look to be a linear relationship at all.
 
 cor_results <- cor.test(all_data$HDI,all_data$SWLSMean)
 
